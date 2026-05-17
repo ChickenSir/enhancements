6b74634dc7fd7eead3532564b30a00dc12d197b9
ChickenSir <shyguyman11@gmail.com>
2026-05-17 16:18:11 +1000

    Add Block Model datagen files

---
+++ src/client/java/com/vanillaadditions/client/datagen/VAModelProvider.java
@@ -0,0 +1,24 @@
+package com.vanillaadditions.client.datagen;
+
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+
+public class VAModelProvider extends FabricModelProvider {
+
+    public VAModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators itemModelGenerator) {
+        
+    }
+    
+}
