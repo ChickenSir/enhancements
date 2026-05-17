c9cc62b2e44248c8a716c88fe733bbaaac3a4707
ChickenSir <shyguyman11@gmail.com>
2026-05-17 14:45:29 +1000

    Initial project setup

---
+++ src/client/java/com/vanillaadditions/client/VanillaAdditionsClient.java
@@ -0,0 +1,10 @@
+package com.vanillaadditions.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaAdditionsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
