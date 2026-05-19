582c246307e9420b9cb726b92a820c4d6c7a93aa
ChickenSir <shyguyman11@gmail.com>
2026-05-19 13:50:58 +1000

    Update oak log stack rotate when placed

--- src/main/java/com/enhancements/block/LogStackBlock.java
+++ src/main/java/com/enhancements/block/LogStackBlock.java
@@ -1,21 +1,49 @@
 package com.enhancements.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class LogStackBlock extends Block {
     public static final VoxelShape BlockShape = Block.box(0, 0, 0, 16, 8, 16);
+    public static final EnumProperty<Direction> FACING;
 
     public LogStackBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue(FACING, Direction.NORTH));
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        // Returns the block state from the blocks placement direction
+        return (BlockState)this.defaultBlockState().setValue(FACING, blockPlaceContext.getHorizontalDirection().getClockWise());
     }
     
     protected VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext collisionContext) {
         return BlockShape;
+    }
+
+    protected BlockState rotate(BlockState blockState, Rotation rotation) {
+        // Updates the facing property based on direction
+        return (BlockState)blockState.setValue(FACING, rotation.rotate((Direction)blockState.getValue(FACING)));
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        // Adds the facing property to the block state definition
+        builder.add(new Property[]{FACING});
+    }
+
+    static {
+        FACING = HorizontalDirectionalBlock.FACING;
     }
 
 }
